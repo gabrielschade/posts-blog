1722222421c96411e7ac74461736f9a03e182cc6
Gabriel Schade <re035148@ITAW10N07304>
2018-01-25 15:35:01 -0200

    Exemplo de método de extensão.

---
+++ ExtensionMethods/Extensao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionMethods
+{
+    public static class Extensao
+    {
+        public static bool Par(this int numero)
+        {
+            return numero % 2 == 0;
+        }
+
+        public static bool MaiorQue(this int numero, int segundoNumero)
+        {
+            return numero > segundoNumero;
+        }
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Extensao.Par(3);         // -> false
+            Extensao.MaiorQue(3, 2); // -> true
+
+            int numero = 3;
+            numero.Par();       // -> false
+            numero.MaiorQue(2); // -> true
+        }
+    }
+}
