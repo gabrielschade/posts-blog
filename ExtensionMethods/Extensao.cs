ec04c40bc14973c4c5ae6b59774d7e2ec0af8f78
Gabriel Schade <re035148@ITAW10N07304>
2018-01-25 15:39:09 -0200

    Inclusão de arrow functions

--- ExtensionMethods/Extensao.cs
+++ ExtensionMethods/Extensao.cs
@@ -7,13 +7,11 @@ namespace ExtensionMethods
     public static class Extensao
     {
         public static bool Par(this int numero)
-        {
-            return numero % 2 == 0;
-        }
+            => numero % 2 == 0;
+        
 
         public static bool MaiorQue(this int numero, int segundoNumero)
-        {
-            return numero > segundoNumero;
-        }
+            => numero > segundoNumero;
+        
     }
 }
