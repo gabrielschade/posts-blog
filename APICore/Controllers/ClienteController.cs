8ad20f2e0af0a097c1fa1d6fda6fbe11681a4303
Gabriel Schade <gabriel.schade@hotmail.com>
2018-09-25 10:03:27 -0300

    Atualização para incluir os ResponseTypes

--- APICore/Controllers/ClienteController.cs
+++ APICore/Controllers/ClienteController.cs
@@ -19,5 +19,54 @@ namespace APICore.Controllers
         [HttpGet]
         public Guid ObterId(string CPF)
         => Guid.NewGuid();
+
+        /// <summary>
+        /// Atualiza o cliente informado no parâmetro
+        /// </summary>
+        /// <param name="clienteRequest">Informações para atualizar o cliente</param>
+        /// <returns>
+        /// Retorna o response do cliente atualizado
+        /// </returns>
+        /// <response code="200">Cliente Atualizado</response>
+        /// <response code="404">Não foi possível encontrar um cliente com o Id informado</response>
+        /// <response code="500">Erro no servidor</response>
+        [HttpPost]
+        [ProducesResponseType(typeof(ClienteResponse), 200)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public IActionResult Atualizar(ClienteRequest clienteRequest)
+        {
+            if (clienteRequest.Id != Guid.Empty)
+                return Ok(new ClienteResponse()); 
+            else
+                return NotFound();
+        }
+    }
+
+    public class ClienteRequest
+    {
+        /// <summary>
+        /// Id do cliente que será atualizado
+        /// </summary>
+        public Guid Id { get; set; }
+        /// <summary>
+        /// Nome do cliente
+        /// </summary>
+        public string Nome { get; set; }
+        /// <summary>
+        /// Sobrenome do cliente
+        /// </summary>
+        public string Sobrenome { get; set; }
+        /// <summary>
+        /// Data de nascimento do cliente
+        /// </summary>
+        public DateTime DataNascimento { get; set; }
+    }
+
+    public class ClienteResponse
+    {
+        public Guid Id { get; set; }
+        public string NomeCompleto { get; set; }
+        public DateTime DataNascimento { get; set; }
     }
 }
