66a60fb787e33861aaa98ebc2e101f29e2bce26a
Gabriel Schade <gabriel.schade@hotmail.com>
2019-01-14 12:11:05 -0200

    Criação do código para o post sobre IEnumerable

---
+++ IEnumerableSamples/IEnumerableSamples/InfinityNumbersList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IEnumerableSamples
+{
+    public class InfinityNumbersList : IEnumerable<int>
+    {
+        private readonly InfinityNumbersListEnumerator _enumerator;
+        
+        public InfinityNumbersList()
+        {
+            _enumerator = new InfinityNumbersListEnumerator();
+        }
+        public IEnumerator<int> GetEnumerator()
+        => _enumerator;
+
+        IEnumerator IEnumerable.GetEnumerator()
+        => _enumerator;
+    }
+
+    public class InfinityNumbersListEnumerator : IEnumerator<int>
+    {
+        private int _current = -1;
+        public int Current => _current;
+
+        object IEnumerator.Current => _current;
+        public bool MoveNext()
+        {
+            _current++;
+            return true;
+        }
+
+        public void Reset()
+        {
+            _current = -1;
+        }
+
+        public void Dispose()
+        {
+            Reset();
+        }
+    }
+}
---
+++ IEnumerableSamples/IEnumerableSamples/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IEnumerableSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InfinityNumbersList allNumbers = new InfinityNumbersList();
+            using (IEnumerator<int> enumerator = allNumbers.GetEnumerator())
+            {
+                while (enumerator.MoveNext())
+                {
+                    int value = enumerator.Current;
+                    Console.WriteLine(value);
+
+                    if (value >= 100)
+                        break;
+                }
+            }
+
+            foreach (int number in allNumbers)
+            {
+                Console.WriteLine(number);
+
+                if (number >= 100)
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
