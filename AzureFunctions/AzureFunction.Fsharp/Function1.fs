f39f217114d618b53268af3d960e2dd4a86df318
Gabriel Schade <gabriel.schade@hotmail.com>
2019-05-29 21:59:06 +0100

    Código do post de Azure Function

---
+++ AzureFunctions/AzureFunction.Fsharp/Function1.fs
@@ -0,0 +1,45 @@
+﻿namespace FunctionApp1
+
+open System
+open Microsoft.Azure.WebJobs
+open Microsoft.Azure.WebJobs.Host
+open Microsoft.Azure.WebJobs.Extensions.Http
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.Logging;
+open System.IO
+open Newtonsoft.Json
+open Microsoft.AspNetCore.Mvc
+open FSharp.Data
+
+module Function1 =
+
+    [<FunctionName("Function1FSharp")>]
+    let DefaultRun ([<HttpTrigger(AuthorizationLevel.Function, "get", Route = null)>] request: HttpRequest ) 
+            (log: ILogger) =
+        log.LogInformation "F# HTTP trigger function processed a request."
+        let name = if request.Query.ContainsKey "name"
+                   then request.Query.["name"].ToString() |> Some
+                   else None
+
+        match name with
+        | Some name -> OkObjectResult (sprintf "Hello, %s" name) :> IActionResult
+        | None -> BadRequestObjectResult "Please pass a name on the query string or in the request body" :> IActionResult
+        
+    [<Literal>]
+    let url = "https://en.wikipedia.org/wiki/List_of_Marvel_Cinematic_Universe_films"
+    type MarvelCinematicPage = HtmlProvider<url>
+       
+    type Film = { 
+        Name:string; 
+        Critical: string
+    }
+
+    [<FunctionName("FunctionTypeProviderFSharp")>]
+    let Run ([<HttpTrigger(AuthorizationLevel.Function, "get", Route = null)>] request: HttpRequest ) 
+            (log: ILogger) =
+        let page = MarvelCinematicPage.Load url
+
+        page.Tables.``Critical response``.Rows
+        |> Seq.filter( fun data -> data.Film <> "Average")
+        |> Seq.map (fun data -> {Name = data.Film; Critical = data.``Rotten Tomatoes``})
+        |> OkObjectResult
---
+++ AzureFunctions/AzureFunction/Function1.cs
@@ -0,0 +1,28 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AzureFunction
+{
+        public static class Function1
+        {
+            [FunctionName("Function1")]
+            public static IActionResult Run(
+                [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+                ILogger log)
+            {
+                log.LogInformation("C# HTTP trigger function processed a request.");
+
+                string name = req.Query["name"];
+                return name != null
+                    ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                    : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            }
+        }
+}
